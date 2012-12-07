154c438b83dec5399875008a0d9e7d0ff897b04f
Felix Geisendörfer <felix@debuggable.com>
2012-12-07 09:31:14 +0100

    More comments

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
-	"time"
+	//"time"
 	"github.com/felixge/go-ardrone-firmware/navdata"
+	"io"
+	"os"
 )
 
 func main() {
@@ -15,19 +17,35 @@ func main() {
 		panic(err)
 	}
 
-	start := time.Now()
-	for{
-		data, err := navdataReader.ReadNavdata()
-		if err != nil {
-			panic(err)
-		}
-
-		log.Printf("navdata: %+v\n", data)
-
-		if time.Since(start) > 30 * time.Second {
-			break
-		}
+	fixture, err := os.OpenFile(
+		"/data/video/navdata.bin",
+		os.O_RDWR|os.O_CREATE|os.O_TRUNC,
+		0,
+	)
+	if err != nil {
+		panic(err)
 	}
+
+	n, err := io.Copy(fixture, navdataReader)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Wrote %d bytes\n", n)
+
+	//start := time.Now()
+	//for{
+	//data, err := navdataReader.ReadNavdata()
+	//if err != nil {
+	//panic(err)
+	//}
+
+	//log.Printf("navdata: %+v\n", data)
+
+	//if time.Since(start) > 30 * time.Second {
+	//break
+	//}
+	//}
 
 	log.Println("quit")
 }
--- navdata/navdata.go
+++ navdata/navdata.go
@@ -7,11 +7,11 @@ import (
 	"os"
 )
 
-type Reader struct{
+type Decoder struct{
 	file *os.File
 }
 
-func NewReader() (*Reader, error) {
+func NewDecoder() (*Decoder, error) {
 	// TODO set baudrate and other options (right now we get away without because
 	// the official firmware configures the device before we kill it)
 	file, err := os.OpenFile("/dev/ttyO1", os.O_RDWR, 0)
@@ -19,28 +19,30 @@ func NewReader() (*Reader, error) {
 		return nil, err
 	}
 
-	reader := &Reader{}
-	reader.file = file
-	return reader, nil
+	decoder := &Decoder{}
+	decoder.file = file
+	return decoder, nil
 }
 
 // TODO figure out the correct structure coming from the tty (seems like
-// AccTemp / GyroTemp are related to Ultrasound.
+// AccTemp / GyroTemp are related to Ultrasound and not actually what they
+// were on the 1.0 board.
 type Navdata struct{
 	Seq uint16
-	Acc [3]uint16
-	Gyro [3]uint16
-	Gyro110 [2]uint16
-	AccTemp uint16
-	GyroTemp uint16
-	Ultrasound uint16
+	Rest [56]byte
+	//Acc [3]uint16
+	//Gyro [3]uint16
+	//Gyro110 [2]uint16
+	//AccTemp uint16
+	//GyroTemp uint16
+	//Ultrasound uint16
 }
 
 // ReadNavdata reads a single Navdata struct from the navdata serialport.
-func (r *Reader) ReadNavdata() (*Navdata, error) {
+func (d *Decoder) ReadNavdata() (*Navdata, error) {
 	var size uint16
 	for {
-		err := binary.Read(r.file, binary.LittleEndian, &size)
+		err := binary.Read(d.file, binary.LittleEndian, &size)
 		if err != nil {
 			return nil, err
 		}
@@ -50,15 +52,15 @@ func (r *Reader) ReadNavdata() (*Navdata, error) {
 	}
 
 	raw := make([]byte, size)
-	_, err := io.ReadAtLeast(r.file, raw, int(size))
+	_, err := io.ReadAtLeast(d.file, raw, int(size))
 	if err != nil {
 		return nil, err
 	}
 
 	data := &Navdata{}
-	reader := bytes.NewReader(raw)
+	decoder := bytes.NewReader(raw)
 
-	err = binary.Read(reader, binary.LittleEndian, data)
+	err = binary.Read(decoder, binary.LittleEndian, data)
 	if err != nil{
 		return nil, err
 	}
